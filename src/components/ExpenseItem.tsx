fd69014aad0c8c9a0c0e9e8ed63154b0eae05cda
Rolandas Ananijevas <rolandas.ananijevas@juvare.com>
2021-10-21 20:03:28 +0300

    props values used inline

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 import './ExpenseItem.css';
 
 export const ExpenseItem: React.FC<{ title: string, amount: number, date: Date }> = (props) => {
-    const {date: expenseDate, title: expenseDescription, amount: expenseAmount} = props;
-
     return (
         <div data-testid='expense-item' className='expense-item'>
-            <div data-testid='expense-item-date'>{expenseDate.toISOString()}</div>
+            <div data-testid='expense-item-date'>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2 data-testid='expense-item-description'>{expenseDescription}</h2>
+                <h2 data-testid='expense-item-description'>{props.title}</h2>
                 <div data-testid='expense-item-price' className='expense-item__price'>
-                    €{expenseAmount}
+                    €{props.amount}
                 </div>
             </div>
         </div>
