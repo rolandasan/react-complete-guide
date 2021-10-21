03df080eca0e2946ae689bc2d7063358c637801a
Rolandas Ananijevas <rolandas.ananijevas@juvare.com>
2021-10-21 20:39:24 +0300

    Added date formatting and updated tests accordingly

--- src/components/ExpenseItem.test.tsx
+++ src/components/ExpenseItem.test.tsx
@@ -3,10 +3,30 @@ import {render, screen} from '@testing-library/react';
 import {ExpenseItem} from "./ExpenseItem";
 
 test('render ExpenseItem component', () => {
-    render(<ExpenseItem/>);
-    const testedElement = screen.getByTestId('expense-item');
-    expect(testedElement).toBeInTheDocument();
-    expect(screen.getByTestId('expense-item-description')).toBeInTheDocument();
-    expect(screen.getByTestId('expense-item-date')).toBeInTheDocument();
-    expect(screen.getByTestId('expense-item-price')).toBeInTheDocument();
+    render(<ExpenseItem title='expense title' amount={10.4} date={new Date(2021, 10, 12, 5,13)}/>);
+    const expenseItemComponent = screen.getByTestId('expense-item');
+    expect(expenseItemComponent).toBeInTheDocument();
+
+    const expenseItemDescription = screen.getByTestId('expense-item-description');
+    expect(expenseItemDescription).toBeInTheDocument();
+    expect(expenseItemDescription.textContent).toBe('expense title');
+
+    const expenseItemDate = screen.getByTestId('expense-item-date');
+    expect(expenseItemDate).toBeInTheDocument();
+
+    const expenseItemDateYear = screen.getByTestId('expense-item-date-year');
+    expect(expenseItemDateYear).toBeInTheDocument();
+    expect(expenseItemDateYear.textContent).toBe('2021');
+
+    const expenseItemDateMonth = screen.getByTestId('expense-item-date-month');
+    expect(expenseItemDateMonth).toBeInTheDocument();
+    expect(expenseItemDateMonth.textContent).toBe('lapkritis');
+
+    const expenseItemDateDay = screen.getByTestId('expense-item-date-day');
+    expect(expenseItemDateDay).toBeInTheDocument();
+    expect(expenseItemDateDay.textContent).toBe('12');
+
+    const expenseItemAmount = screen.getByTestId('expense-item-price')
+    expect(expenseItemAmount).toBeInTheDocument();
+    expect(expenseItemAmount.textContent).toBe('€10.4');
 })
--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -3,9 +3,17 @@ import './ExpenseItem.css';
 import { Expense } from '../models/Expense';
 
 export const ExpenseItem: React.FC<Expense> = (props) => {
+    const expenseDateMonth = props.date.toLocaleDateString('lt-LT', { month: 'long' });
+    const expenseDateYear = props.date.getFullYear();
+    const expenseDateDay = props.date.getDate();
+
     return (
         <div data-testid='expense-item' className='expense-item'>
-            <div data-testid='expense-item-date'>{props.date.toISOString()}</div>
+            <div data-testid='expense-item-date'>
+                <div data-testid='expense-item-date-year'>{expenseDateYear}</div>
+                <div data-testid='expense-item-date-month'>{expenseDateMonth}</div>
+                <div data-testid='expense-item-date-day'>{expenseDateDay}</div>
+            </div>
             <div className='expense-item__description'>
                 <h2 data-testid='expense-item-description'>{props.title}</h2>
                 <div data-testid='expense-item-price' className='expense-item__price'>
