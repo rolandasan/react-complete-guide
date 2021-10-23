2f8f58d1f5ed0c4ba0582fdaf46132726087fdec
Rolandas Ananijevas <rolandas.ananijevas@juvare.com>
2021-10-23 20:28:39 +0300

    Expense input form hidden by default and shown only when button clicked, expense input form hides when new expense added or Cancel button clicked

--- src/components/NewExpense/ExpenseForm.tsx
+++ src/components/NewExpense/ExpenseForm.tsx
@@ -14,6 +14,7 @@ export const ExpenseForm: React.FC<ExpenseFormProps> = (props) => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
+    const [showForm, setShowForm] = useState(false);
 
     const titleChangedHandler = (event: ChangeEvent<HTMLInputElement>): void => {
         setEnteredTitle(event.target.value);
@@ -38,26 +39,62 @@ export const ExpenseForm: React.FC<ExpenseFormProps> = (props) => {
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
+        setShowForm((prev) => !prev);
     };
 
+    const showFormHandler = () => {
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+        setShowForm((prev) => !prev);
+    };
+
+    if (!showForm) {
+        return (
+            <div data-testid='expense-form-hidden'>
+                <button onClick={showFormHandler} data-testid='btn-show-expense-form'>
+                    Add Expense
+                </button>
+            </div>
+        );
+    }
     return (
         <form data-testid='expense-form' onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' value={enteredTitle} onChange={titleChangedHandler} />
+                    <input type='text' value={enteredTitle} onChange={titleChangedHandler} data-testid='inp-expense-title' />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' value={enteredAmount} min={0.01} step={0.01} onChange={amountChangedHandler} />
+                    <input
+                        type='number'
+                        value={enteredAmount}
+                        min={0.01}
+                        step={0.01}
+                        onChange={amountChangedHandler}
+                        data-testid='inp-expense-amount'
+                    />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' value={enteredDate} min='2019-01-01' max='2022-12–31' onChange={dateChangedHandler} />
+                    <input
+                        type='date'
+                        value={enteredDate}
+                        min='2019-01-01'
+                        max='2022-12–31'
+                        onChange={dateChangedHandler}
+                        data-testid='inp-expense-date'
+                    />
                 </div>
             </div>
             <div className='new-expense__actions'>
-                <button type='submit'>Add Expense</button>
+                <button type='reset' onClick={showFormHandler} data-testid='btn-close-expense-form'>
+                    Cancel
+                </button>
+                <button type='submit' data-testid='btn-submit-expense-form'>
+                    Add Expense
+                </button>
             </div>
         </form>
     );
