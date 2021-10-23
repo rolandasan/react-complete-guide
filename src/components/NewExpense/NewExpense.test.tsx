724bb2e366e535418643175ed52d9f32eb4e2217
Rolandas Ananijevas <rolandas.ananijevas@juvare.com>
2021-10-23 21:51:07 +0300

    Added missing props to component in tests

--- src/components/NewExpense/NewExpense.test.tsx
+++ src/components/NewExpense/NewExpense.test.tsx
@@ -2,8 +2,8 @@ import { NewExpense } from './NewExpense';
 import { render, screen } from '@testing-library/react';
 
 test('render NewExpense component', () => {
-    render(<NewExpense />);
+    render(<NewExpense onAddExpense={() => {}} />);
     const newExpenseComponent = screen.getByTestId('new-expense');
-    expect(newExpenseComponent).toBeInTheDocument()
+    expect(newExpenseComponent).toBeInTheDocument();
     expect(newExpenseComponent.className).toBe('new-expense');
 });
