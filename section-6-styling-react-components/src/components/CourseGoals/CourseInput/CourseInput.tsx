c918e1a526babec14e990309afebe6e43f6367d9
Rolandas Ananijevas <rolandas.ananijevas@juvare.com>
2021-10-24 22:31:23 +0300

    Dynamic CSS class

--- section-6-styling-react-components/src/components/CourseGoals/CourseInput/CourseInput.tsx
+++ section-6-styling-react-components/src/components/CourseGoals/CourseInput/CourseInput.tsx
@@ -29,13 +29,9 @@ export const CourseInput: React.FC<CourseInputProps> = (props) => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className='form-control'>
-                <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
-                <input
-                    style={{ borderColor: !isValid ? 'red' : 'black', backgroundColor: !isValid ? 'lightsalmon' : 'transparent' }}
-                    type='text'
-                    onChange={goalInputChangeHandler}
-                />
+            <div className={`form-control${!isValid ? ' invalid' : ''}`}>
+                <label>Course Goal</label>
+                <input type='text' onChange={goalInputChangeHandler} />
             </div>
             <Button type='submit'>Add Goal</Button>
         </form>
