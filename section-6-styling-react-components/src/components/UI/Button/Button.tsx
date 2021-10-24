9566b4215da11cfad002d5cebd6ed083e29b119d
Rolandas Ananijevas <rolandas.ananijevas@juvare.com>
2021-10-24 22:55:25 +0300

    Introduced styled components

--- section-6-styling-react-components/src/components/UI/Button/Button.tsx
+++ section-6-styling-react-components/src/components/UI/Button/Button.tsx
@@ -1,16 +1,23 @@
-import React, { FormEvent, PropsWithChildren } from 'react';
-
 import './Button.css';
+import styled from 'styled-components';
 
-export const Button: React.FC<
-    PropsWithChildren<{
-        type: 'submit' | 'reset' | 'button';
-        onClick?: (event: FormEvent) => void;
-    }>
-> = (props) => {
-    return (
-        <button type={props.type} className='button' onClick={props.onClick}>
-            {props.children}
-        </button>
-    );
-};
+export const Button = styled.button`
+    font: inherit;
+    padding: 0.5rem 1.5rem;
+    border: 1px solid #8b005d;
+    color: white;
+    background: #8b005d;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+    cursor: pointer;
+
+    &:focus {
+        outline: none;
+    }
+
+    &:hover,
+    &:active {
+        background: #ac0e77;
+        border-color: #ac0e77;
+        box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+    }
+`;
