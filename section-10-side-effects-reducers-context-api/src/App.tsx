beddf3b93d03ac7827b0214937bd0967574e7922
Rolandas Ananijevas <rolandas.ananijevas@juvare.com>
2021-10-27 21:04:29 +0300

    Added useEffect function to run some code once at startup

--- section-10-side-effects-reducers-context-api/src/App.tsx
+++ section-10-side-effects-reducers-context-api/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MainHeader } from "./components/MainHeader/MainHeader";
 import { Login } from "./components/Login/Login";
 import { Home } from "./components/Home/Home";
@@ -6,11 +6,18 @@ import { Home } from "./components/Home/Home";
 export const App: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    if (localStorage.getItem("isLoggedIn") === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
   const loginHandler = (email: string, password: string) => {
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(true);
   };
 
