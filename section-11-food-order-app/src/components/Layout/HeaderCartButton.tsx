d939fe9fc97b983e523d2ac8d687a2a5c00ee65d
Rolandas Ananijevas <rolandas.ananijevas@juvare.com>
2021-10-28 23:46:01 +0300

    Added HeaderCartButton component

---
+++ section-11-food-order-app/src/components/Layout/HeaderCartButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './HeaderCartButton.module.css';
+import { CartIcon } from '../Cart/CartIcon';
+
+export const HeaderCartButton: React.FC = () => {
+    return (
+        <button className={classes.button}>
+            <span className={classes.icon}>
+                <CartIcon />
+            </span>
+            <span>Your Cart</span>
+            <span className={classes.badge}>2</span>
+        </button>
+    );
+};
