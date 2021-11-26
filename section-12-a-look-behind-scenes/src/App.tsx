82c4e13878cc7f1c1963f361b40e223ecf4266f8
Rolandas Ananijevas <rolandas.ananijevas@juvare.com>
2021-11-27 00:15:33 +0200

    Added additional paragraph and button to toggle it

--- section-12-a-look-behind-scenes/src/App.tsx
+++ section-12-a-look-behind-scenes/src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './App.css';
+import {Button} from "./components/UI/Button/Button";
 
-export const App:React.FC = () => {
+export const App: React.FC = () => {
+    const [showParagraph, setShowParagraph] = useState(false);
+
+    const toggleParagraphHandler = () => {
+        setShowParagraph(prevShowParagraph => !prevShowParagraph);
+    }
+
     return (
         <div className='app'>
             <h1>Hi there!</h1>
+            {showParagraph && <p>This is new!</p>}
+            <Button onClick={toggleParagraphHandler}>Toggle paragraph</Button>
         </div>
     );
 };
