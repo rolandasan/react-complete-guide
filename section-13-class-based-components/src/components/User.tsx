8616284a076e9c2dc27dcf232c34538e5533b05a
Rolandas Ananijevas <rolandas.ananijevas@juvare.com>
2021-11-30 21:29:50 +0200

    User component converted to class based component

--- section-13-class-based-components/src/components/User.tsx
+++ section-13-class-based-components/src/components/User.tsx
@@ -1,10 +1,20 @@
 import classes from './User.module.css';
-import React from 'react';
+import React, { Component } from 'react';
 
 interface Props {
     name: string;
 }
 
-export const User: React.FC<Props> = (props) => {
-    return <li className={classes.user}>{props.name}</li>;
-};
+export class User extends Component<Props, {}> {
+    constructor(props: Props) {
+        super(props);
+    }
+
+    render() {
+        return <li className={classes.user}>{this.props.name}</li>;
+    }
+}
+
+// export const User: React.FC<Props> = (props) => {
+//     return <li className={classes.user}>{props.name}</li>;
+// };
