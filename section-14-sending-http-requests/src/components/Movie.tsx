601635a015633d989133dd15dc63dc8794cd0dc1
Rolandas Ananijevas <rolandas.ananijevas@juvare.com>
2021-12-01 00:48:03 +0200

    Application converted to Typescript

---
+++ section-14-sending-http-requests/src/components/Movie.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import classes from './Movie.module.css';
+
+interface Props {
+    title: string;
+    releaseDate: string;
+    openingText: string;
+}
+
+export const Movie: React.FC<Props> = (props) => {
+    return (
+        <li className={classes.movie}>
+            <h2>{props.title}</h2>
+            <h3>{props.releaseDate}</h3>
+            <p>{props.openingText}</p>
+        </li>
+    );
+};
+
+export default Movie;
---
+++ section-14-sending-http-requests/src/components/MoviesList.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Movie } from './Movie';
+import classes from './MoviesList.module.css';
+
+interface Props {
+    movies: { id: number; title: string; openingText: string; releaseDate: string }[];
+}
+
+export const MovieList: React.FC<Props> = (props) => {
+    return (
+        <ul className={classes['movies-list']}>
+            {props.movies.map((movie) => (
+                <Movie key={movie.id} title={movie.title} releaseDate={movie.releaseDate} openingText={movie.openingText} />
+            ))}
+        </ul>
+    );
+};
+
+export default MovieList;
