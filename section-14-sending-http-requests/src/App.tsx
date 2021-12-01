b6018e5bfd74430bb7b261aab174756007c54c77
Rolandas Ananijevas <rolandas.ananijevas@juvare.com>
2021-12-01 02:01:37 +0200

    Added errors handling and useEffect

--- section-14-sending-http-requests/src/App.tsx
+++ section-14-sending-http-requests/src/App.tsx
@@ -1,26 +1,43 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { MoviesList } from './components/MoviesList';
 import './App.css';
 
 export const App: React.FC = () => {
     const [movies, setMovies] = useState<{ id: number; title: string; openingText: string; releaseDate: string }[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState<any>(null);
 
-    const fetchMoviesHandler = async () => {
-        const response = await fetch('https://swapi.dev/api/films');
-        const data = await response.json();
-        const transformedMovies = data.results.map(
-            (movie: { episode_id: number; title: string; opening_crawl: string; release_date: string }) => {
-                return {
-                    id: movie.episode_id,
-                    title: movie.title,
-                    openingText: movie.opening_crawl,
-                    releaseDate: movie.release_date,
-                };
-            },
-        );
-        setMovies(transformedMovies);
-    };
+    const fetchMoviesHandler = useCallback(async () => {
+        setIsLoading(true);
+        setIsError(null);
+        try {
+            const response = await fetch('https://swapi.dev/api/films/');
+            if (!response.ok) {
+                throw new Error('Something wrong');
+            }
+
+            const data = await response.json();
+            const transformedMovies = data.results.map(
+                (movie: { episode_id: number; title: string; opening_crawl: string; release_date: string }) => {
+                    return {
+                        id: movie.episode_id,
+                        title: movie.title,
+                        openingText: movie.opening_crawl,
+                        releaseDate: movie.release_date,
+                    };
+                },
+            );
+            setMovies(transformedMovies);
+        } catch (error) {
+            setIsError((error as Error).message);
+        }
+        setIsLoading(false);
+    }, []);
+
+    useEffect(() => {
+        fetchMoviesHandler();
+    }, [fetchMoviesHandler]);
 
     return (
         <>
@@ -28,7 +45,10 @@ export const App: React.FC = () => {
                 <button onClick={fetchMoviesHandler}>Fetch Movies</button>
             </section>
             <section>
-                <MoviesList movies={movies} />
+                {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+                {!isLoading && movies.length === 0 && !isError && <p>No movies</p>}
+                {isLoading && <p>Loading data</p>}
+                {!isLoading && isError && <p>{isError}</p>}
             </section>
         </>
     );
