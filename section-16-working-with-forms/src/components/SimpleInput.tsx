43d0027368f9aefc1931168348aa4c94ef5ec8ce
Rolandas Ananijevas <rolandas.ananijevas@juvare.com>
2021-12-04 00:50:55 +0200

    Read entered name using inout onChange event

--- section-16-working-with-forms/src/components/SimpleInput.tsx
+++ section-16-working-with-forms/src/components/SimpleInput.tsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useRef, useState } from 'react';
 
-export const SimpleInput:React.FC = () => {
+export const SimpleInput: React.FC = () => {
+    const [enteredName, setEnteredName] = useState('');
+
+    const inputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setEnteredName(event.target.value);
+    };
+
+    const formSubmitHandler = (event: FormEvent) => {
+        event.preventDefault();
+        console.debug(enteredName);
+    };
+
     return (
-        <form>
+        <form onSubmit={formSubmitHandler}>
             <div className='form-control'>
                 <label htmlFor='name'>Your Name</label>
-                <input type='text' id='name' />
+                <input type='text' id='name' onChange={inputChangeHandler} />
             </div>
-            <div className="form-actions">
+            <div className='form-actions'>
                 <button>Submit</button>
             </div>
         </form>
     );
-}
+};
