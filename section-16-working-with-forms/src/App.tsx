244a1d1790b80b9e0a956dc4ee05a477529b3093
Rolandas Ananijevas <rolandas.ananijevas@juvare.com>
2021-12-04 00:35:27 +0200

    Initial section 16 application

---
+++ section-16-working-with-forms/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SimpleInput } from './components/SimpleInput';
+
+export const App: React.FC = () => {
+    return (
+        <div className='app'>
+            <SimpleInput />
+        </div>
+    );
+};
