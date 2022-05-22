b91a82689c4d5996cf88d4bc6f7564004e0d0d70
Rolandas Ananijevas <rolandas.ananijevas@juvare.com>
2022-05-22 18:18:08 +0300

    Added updated styles and refs to access entered form values

--- section-17-food-order-http-forms/src/components/Cart/Checkout.tsx
+++ section-17-food-order-http-forms/src/components/Cart/Checkout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import classes from './Checkout.module.css';
 
 interface Props {
@@ -6,31 +6,41 @@ interface Props {
 }
 
 export const Checkout: React.FC<Props> = (props) => {
-    const confirmHandler = (event:any) => {
+    const nameInputRef = useRef<HTMLInputElement>(null);
+    const streetInputRef = useRef<HTMLInputElement>(null);
+    const postalCodeInputRef = useRef<HTMLInputElement>(null);
+    const cityInputRef = useRef<HTMLInputElement>(null);
+    const confirmHandler = (event: any) => {
         event.preventDefault();
+        const enteredName = nameInputRef.current!.value;
+        const enteredStreet = streetInputRef.current!.value;
+        const enteredPostalCode = postalCodeInputRef.current!.value;
+        const enteredCity = cityInputRef.current!.value;
     };
     return (
-        <form onSubmit={confirmHandler}>
+        <form className={classes.form} onSubmit={confirmHandler}>
             <div className={classes.control}>
                 <label htmlFor='name'>Your name</label>
-                <input type='text' id='name' />
+                <input type='text' id='name' ref={nameInputRef} />
             </div>
             <div className={classes.control}>
                 <label htmlFor='street'>Street</label>
-                <input type='text' id='street' />
+                <input type='text' id='street' ref={streetInputRef} />
             </div>
             <div className={classes.control}>
                 <label htmlFor='postal-code'>Postal code</label>
-                <input type='text' id='postal-code' />
+                <input type='text' id='postal-code' ref={postalCodeInputRef} />
             </div>
             <div className={classes.control}>
                 <label htmlFor='city'>City</label>
-                <input type='text' id='city' />
+                <input type='text' id='city' ref={cityInputRef} />
             </div>
-            <button type='button' onClick={props.onCancel}>
-                Cancel
-            </button>
-            <button>Confirm</button>
+            <div className={classes.actions}>
+                <button type='button' onClick={props.onCancel}>
+                    Cancel
+                </button>
+                <button className={classes.submit}>Confirm</button>
+            </div>
         </form>
     );
 };
